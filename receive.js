3c9327e7fb3affd2400467e447c6dab54386d7ec
Daniel Ireson <daniel@djgraphics.co.uk>
2016-11-27 21:49:28 +0000

    queue data with dynamodb

--- receive.js
+++ receive.js
@@ -9,6 +9,7 @@ var validator = require('validator')
 module.exports.handler = (event, context, callback) => {
   var docClient = new AWS.DynamoDB.DocumentClient()
   var form = querystring.parse(event.body)
+  var payload = {}
 
   if (form['_honeypot'] !== undefined && !validator.isEmpty(form['_honeypot'])) {
     callback(null, response(422, 'You shall not pass'))
@@ -18,14 +19,23 @@ module.exports.handler = (event, context, callback) => {
     callback(null, response(422, 'Form not sent, the admin has not set up a send-to address.'))
   }
 
-  // Sanitize the input
-  // Encrypt the data and add to dynamodb
-
-  if (form['_redirect-to'] !== undefined && validator.isURL(form['_redirect-to'])) {
-    callback(null, response(301, 'Form submission successfully made.', form['_redirect-to']))
+  payload.id = uuid.v4()
+  payload.data = {}
+  for (var key in form) {
+    payload.data[key] = validator.trim(form[key])
   }
 
-  callback(null, response(200, 'Form submission successfully made.'))
+  docClient.put({TableName: 'formplug-queue', Item: payload}, (error) => {
+    if (error) {
+      callback(null, response(422, 'Form not sent, there was an error adding it to the database.'))
+    }
+
+    if (form['_redirect-to'] !== undefined && validator.isURL(form['_redirect-to'])) {
+      callback(null, response(301, 'Form submission successfully made.', form['_redirect-to']))
+    }
+
+    callback(null, response(200, 'Form submission successfully made.'))
+  })
 }
 
 function response(statusCode, message, redirect) {
