95b82832fcaea876bb6ac1f7d8a0556ef3d955fd
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-14 17:29:16 +0000

    update receive database.js with callback

--- receive/database.js
+++ receive/database.js
@@ -4,11 +4,11 @@ var uuid = require('uuid')
 var config = require('../config.json')
 var encryption = require('../lib/encryption')
 
-module.exports.put = function(form) {
+module.exports.put = function(form, callback) {
   var docClient = new AWS.DynamoDB.DocumentClient()
   var payload = {
     id: uuid.v4(),
     data: encryption.encrypt(JSON.stringify(form))
   }
-  return docClient.put({TableName: config.TABLE_NAME, Item: payload})
+  return docClient.put({TableName: config.TABLE_NAME, Item: payload}, callback())
 }
