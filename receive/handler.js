3bcc91bb438dbdf4f77a8e1df990c75e7f41ea37
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-14 20:37:04 +0000

    add response renders for database.put callback

--- receive/handler.js
+++ receive/handler.js
@@ -2,12 +2,24 @@ var querystring = require('querystring')
 
 var database = require('./database')
 var validate = require('./validate')
+var response = require('./response')
 
 module.exports.handle = (event, context, callback) => {
   var data = querystring.parse(event.body)
   validate.all(data, callback)
 
-  database.put(data, function() {
+  database.put(data, function(error) {
+    if (error) {
+      callback(null, response.render('error'))
+      process.exit()
+    }
 
+    if (validate.hasRedirect(data)) {
+      callback(null, response.render('redirect', data['redirect-to']))
+      process.exit()
+    }
+
+    callback(null, response.render('success'))
+    process.exit()
   })
 }
--- receive/response.js
+++ receive/response.js
@@ -3,13 +3,22 @@ var path = require('path')
 
 var config = require('../config.json')
 
-module.exports.render = function(type) {
+module.exports.render = function(type, redirect) {
   switch(type) {
     case 'honeypot': 
       return build(422, config.MSG_HONEYPOT || 'You shall not pass')
       break
     case 'no-admin-email':
       return build(422, config.MSG_NO_ADMIN_EMAIL || 'Form not sent, the admin has not set up a send-to address.')
+      break
+    case 'error':
+      return build(500, config.MSG_ERROR || 'Form not sent, there was an error adding it to the database.')
+      break
+    case 'success':
+      return build(302, config.MSG_SUCCESS || 'Form submission successfully made.')
+      break
+    case 'redirect':
+      return build(200, config.MSG_SUCCESS || 'Form submission successfully made.', redirect)
       break
     default:
       return build(500)
