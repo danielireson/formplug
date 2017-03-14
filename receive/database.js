e2abda55f08ca87ec57fac907734c37fc73bbe56
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-14 17:33:33 +0000

    change form var name to data in receive database.js

--- receive/database.js
+++ receive/database.js
@@ -4,11 +4,11 @@ var uuid = require('uuid')
 var config = require('../config.json')
 var encryption = require('../lib/encryption')
 
-module.exports.put = function(form, callback) {
+module.exports.put = function(data, callback) {
   var docClient = new AWS.DynamoDB.DocumentClient()
   var payload = {
     id: uuid.v4(),
-    data: encryption.encrypt(JSON.stringify(form))
+    data: encryption.encrypt(JSON.stringify(data))
   }
   return docClient.put({TableName: config.TABLE_NAME, Item: payload}, callback())
 }
