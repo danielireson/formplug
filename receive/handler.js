1639e147a06021c208857a9dec004e782aa5a114
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-14 17:56:45 +0000

    move response render configurations into receive render.js

--- receive/handler.js
+++ receive/handler.js
@@ -2,7 +2,6 @@ var querystring = require('querystring')
 
 var validator = require('validator')
 
-var config = require('../config.json')
 var database = require('./database')
 var render = require('./render')
 
@@ -10,12 +9,14 @@ module.exports.handle = (event, context, callback) => {
   var form = querystring.parse(event.body)
 
   if (form['_honeypot'] !== undefined && !validator.isEmpty(form['_honeypot'])) {
-    callback(null, render.response(422, config.MSG_HONEYPOT || 'You shall not pass'))
+    callback(null, render.response('honeypot'))
   }
 
   if (form['_send-to'] === undefined || !validator.isEmail(form['_send-to'])) {
-    callback(null, render.response(422, config.MSG_MISSING_SEND_TO || 'Form not sent, the admin has not set up a send-to address.'))
+    callback(null, render.response('no-admin-email'))
   }
 
-  database.put(form)
+  database.put(form, function() {
+
+  })
 }
--- receive/render.js
+++ receive/render.js
@@ -1,7 +1,22 @@
 var fs = require('fs')
 var path = require('path')
 
-module.exports.response = function(statusCode, message, redirect) {
+var config = require('../config.json')
+
+module.exports.response = function(type) {
+  switch(type) {
+    case 'honeypot': 
+      return buildResponse(422, config.MSG_HONEYPOT || 'You shall not pass')
+      break
+    case 'no-admin-email':
+      return buildResponse(422, config.MSG_MISSING_SEND_TO || 'Form not sent, the admin has not set up a send-to address.')
+      break
+    default:
+      return buildResponse(500)
+  }
+}
+
+function buildResponse(statusCode, message, redirect) {
   var response = {
     statusCode: statusCode,
     headers: {
