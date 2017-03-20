132b6e68ff6aa7469297355199b3aa3ad3b6ea54
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-20 20:43:04 +0000

    add ses mailer module

--- send/handler.js
+++ send/handler.js
@@ -1,13 +1,18 @@
 'use strict'
 
-module.exports.handle = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'This will receive the sending of events',
-      input: event
-    })
-  }
+const encryption = require('../lib/encryption')
+const mailer = require('./mailer')
 
-  callback(null, response)
+module.exports.handle = (event, context, callback) => {
+  let eventData = event.Records[0].dynamodb.NewImage.data.S
+  let data = encryption.decrypt(eventData)
+  mailer.send(data, function (error) {
+    if (error) {
+      console.error('Error sending email for ' + data['_to'])
+      console.error(error)
+      console.error(data)
+    } else {
+      console.log('Email successfully sent to ' + data['_to'])
+    }
+  })
 }
---
+++ send/mailer.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const AWS = require('aws-sdk')
+
+const config = require('../config.json')
+
+module.exports.send = function (data, callback) {
+  let sesClient = new AWS.SES()
+  return sesClient.sendEmail(buildEmail(data), (error) => callback(error))
+}
+
+function buildEmail (data) {
+  return {
+    Source: config.FROM_EMAIL,
+    Destination: {
+      ToAddresses: [
+        data['_to']
+      ]
+    },
+    Message: {
+      Subject: {
+        Data: 'You have a form submission'
+      },
+      Body: {
+        Text: {
+          Data: buildMessage(data)
+        }
+      }
+    }
+  }
+}
+
+function buildMessage (data) {
+  let message = ''
+  for (let field in data) {
+    // Don't send private variables prefixed with an underscore
+    if (field.slice(0, 1) !== '_') {
+      message += field.toUpperCase() + ': ' + data[field] + '\r\n'
+    }
+  }
+  return message
+}
