b7974b755e51fbf224f8026ae7aa3a0052ccefc7
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-21 20:46:17 +0000

    use database delete function in send handler

--- send/handler.js
+++ send/handler.js
@@ -1,18 +1,28 @@
 'use strict'
 
+const database = require('../lib/database')
 const encryption = require('../lib/encryption')
 const mailer = require('./mailer')
 
 module.exports.handle = (event, context, callback) => {
-  let eventData = event.Records[0].dynamodb.NewImage.data.S
-  let data = encryption.decrypt(eventData)
+  let eventData = event.Records[0].dynamodb.NewImage
+  let id = eventData.id.S
+  let data = encryption.decrypt(eventData.data.S)
   mailer.send(data, function (error) {
     if (error) {
       console.error('Error sending email for ' + data['_to'])
       console.error(error)
       console.error(data)
     } else {
-      console.log('Email successfully sent to ' + data['_to'])
+      console.log('Successfully sent email to ' + data['_to'])
+      database.delete(id, function (error) {
+        if (error) {
+          console.error('Error deleting from queue for ' + id)
+          console.error(error)
+        } else {
+          console.log('Successfully deleted queue item')
+        }
+      })
     }
   })
 }
