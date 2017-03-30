f593ac42680ed783b08f4b93c567797ad413bca5
Daniel Ireson <danielireson@gmail.com>
2017-03-30 17:02:44 +0100

    update receive test to evaluate on returned response object

--- test/receive.js
+++ test/receive.js
@@ -7,7 +7,9 @@ const sinon = require('sinon')
 
 const database = require('../lib/storage/database')
 const route = require('../lib/http/route')
+const response = require('../lib/http/response')
 const receive = require('../handlers/receive/handler')
+const request = require('../handlers/receive/request')
 
 const eventReceiveSuccess = require('../events/receive-success.json')
 const eventReceiveSuccessJson = require('../events/receive-success-json.json')
@@ -18,7 +20,7 @@ const eventHoneypot = require('../events/receive-honeypot.json')
 describe('receive', function () {
   var spy, stub
   beforeEach(function () {
-    spy = sinon.spy(route, 'render')
+    spy = sinon.spy(response, 'build')
     stub = sinon.stub(database, 'put').callsFake((data, callback) => callback())
   })
   afterEach(function () {
@@ -27,27 +29,33 @@ describe('receive', function () {
   })
   describe('success', function () {
     it('html', function () {
-      routeRenderAssert('receive-success', eventReceiveSuccess, spy)
+      responseAssert('receive-success', eventReceiveSuccess, spy)
     })
     it('json', function () {
-      routeRenderAssert('receive-success', eventReceiveSuccessJson, spy)
+      responseAssert('receive-success', eventReceiveSuccessJson, spy)
     })
   })
   describe('error', function () {
     it('bad email', function () {
-      routeRenderAssert('receive-bad-email', eventBadEmail, spy)
+      responseAssert('receive-bad-email', eventBadEmail, spy)
     })
     it('no email', function () {
-      routeRenderAssert('receive-no-email', eventNoEmail, spy)
+      responseAssert('receive-no-email', eventNoEmail, spy)
     })
     it('honeypot', function () {
-      routeRenderAssert('receive-honeypot', eventHoneypot, spy)
+      responseAssert('receive-honeypot', eventHoneypot, spy)
     })
   })
 })
 
-function routeRenderAssert (route, event, spy) {
+function responseAssert (type, event, spy) {
+  let data = request.getParams(event)
+  let routeDetails = route.getRouteDetails(type, data)
   receive.handle(event, {}, sinon.stub())
   assert(spy.calledOnce)
-  assert(spy.calledWith(route))
+  let result = spy.firstCall.returnValue
+  assert.equal(result.statusCode, routeDetails.statusCode)
+  let expectedContentType = data['_format'] === 'json' ? 'application/json' : 'text/html'
+  assert.equal(result.headers['Content-Type'], expectedContentType)
+  assert.include(result.body, routeDetails.message)
 }
