595155f760e33fa7f598e2d7d22dab2694efd097
Daniel Ireson <daniel@djgraphics.co.uk>
2017-03-30 12:48:34 +0100

    add http receive test

--- test/receive.js
+++ test/receive.js
@@ -1,18 +1,53 @@
 const describe = require('mocha').describe
 const it = require('mocha').it
+const beforeEach = require('mocha').beforeEach
+const afterEach = require('mocha').afterEach
 const assert = require('chai').assert
+const sinon = require('sinon')
 
+const database = require('../lib/storage/database')
+const route = require('../lib/http/route')
 const receive = require('../receive/handler')
-const event = require('../events/receive-success-json.json')
+
+const eventReceiveSuccess = require('../events/receive-success.json')
+const eventReceiveSuccessJson = require('../events/receive-success-json.json')
+const eventBadEmail = require('../events/receive-bad-email.json')
+const eventNoEmail = require('../events/receive-no-email.json')
+const eventHoneypot = require('../events/receive-honeypot.json')
 
 describe('receive', function () {
+  var spy, stub
+  beforeEach(function () {
+    spy = sinon.spy(route, 'render')
+    stub = sinon.stub(database, 'put').callsFake((data, callback) => callback())
+  })
+  afterEach(function () {
+    spy.restore()
+    stub.restore()
+  })
   describe('success', function () {
+    it('html', function () {
+      routeRenderAssert('receive-success', eventReceiveSuccess, spy)
+    })
     it('json', function () {
-      let response = receive.handle(event, null, callback)
+      routeRenderAssert('receive-success', eventReceiveSuccessJson, spy)
+    })
+  })
+  describe('error', function () {
+    it('bad email', function () {
+      routeRenderAssert('receive-bad-email', eventBadEmail, spy)
+    })
+    it('no email', function () {
+      routeRenderAssert('receive-no-email', eventNoEmail, spy)
+    })
+    it('honeypot', function () {
+      routeRenderAssert('receive-honeypot', eventHoneypot, spy)
     })
   })
 })
 
-function callback () {
-  return 'called'
+function routeRenderAssert (route, event, spy) {
+  receive.handle(event, {}, sinon.stub())
+  assert(spy.calledOnce)
+  assert(spy.calledWith(route))
 }
