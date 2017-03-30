591a40b0cb3606eae0ede9a496d089267cd67ebe
Daniel Ireson <danielireson@gmail.com>
2017-03-30 22:47:01 +0100

    add encrypt success tests

--- test/index.js
+++ test/index.js
@@ -6,16 +6,20 @@ const assert = require('chai').assert
 const sinon = require('sinon')
 
 const database = require('../lib/storage/database')
+const encryption = require('../lib/storage/encryption')
 const route = require('../lib/http/route')
 const response = require('../lib/http/response')
 const receiveHandler = require('../handlers/receive/handler')
 const receiveRequest = require('../handlers/receive/request')
+const encryptHandler = require('../handlers/encrypt/handler')
 
 const eventReceiveSuccess = require('../events/receive-success.json')
 const eventReceiveSuccessJson = require('../events/receive-success-json.json')
 const eventReceiveBadEmail = require('../events/receive-bad-email.json')
 const eventReceiveNoEmail = require('../events/receive-no-email.json')
 const eventReceiveHoneypot = require('../events/receive-honeypot.json')
+const eventEncryptSuccess = require('../events/encrypt-success.json')
+const eventEncryptSuccessJson = require('../events/encrypt-success-json.json')
 
 describe('receive', function () {
   var spy, stub
@@ -48,14 +52,47 @@ describe('receive', function () {
   })
 })
 
+describe('encrypt', function () {
+  var spy
+  beforeEach(function () {
+    spy = sinon.spy(response, 'build')
+  })
+  afterEach(function () {
+    spy.restore()
+  })
+  describe('success', function () {
+    it('html', function () {
+      responseAssert('encrypt-success', eventEncryptSuccess, spy)
+    })
+    it('json', function () {
+      responseAssert('encrypt-success', eventEncryptSuccessJson, spy)
+    })
+  })
+})
+
 function responseAssert (type, event, spy) {
   let data = receiveRequest.getParams(event)
+  switch (type.split('-')[0]) {
+    case 'receive':
+      receiveHandler.handle(event, {}, sinon.stub())
+      break
+    case 'encrypt':
+      encryptHandler.handle(event, {}, sinon.stub())
+      data['_encrypted'] = encryption.encrypt(data['_email'])
+      break
+    default:
+      logErrorAndExit('Route not recognised')
+  }
   let routeDetails = route.getRouteDetails(type, data)
-  receiveHandler.handle(event, {}, sinon.stub())
   assert(spy.calledOnce)
   let result = spy.firstCall.returnValue
   assert.equal(result.statusCode, routeDetails.statusCode)
   let expectedContentType = data['_format'] === 'json' ? 'application/json' : 'text/html'
   assert.equal(result.headers['Content-Type'], expectedContentType)
   assert.include(result.body, routeDetails.message)
+}
+
+function logErrorAndExit (error) {
+  console.log(error)
+  process.exit(1)
 }
