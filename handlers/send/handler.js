412e3ba8a1d0e774e8799dee1e44e7c7a9004ac4
Daniel Ireson <danielireson@gmail.com>
2017-04-05 11:45:59 +0100

    add renderError func

--- handlers/send/handler.js
+++ handlers/send/handler.js
@@ -3,6 +3,7 @@
 const database = require('../../lib/database/database')
 const log = require('../../lib/utility/log')
 const mail = require('../../lib/mail/mail')
+const route = require('../../lib/http/route')
 const request = require('./request')
 
 module.exports.handle = (event, context, callback) => {
@@ -10,13 +11,13 @@ module.exports.handle = (event, context, callback) => {
     let params = request.getParams(event)
     mail.send(params.data, function (error) {
       if (error) {
-        log.error(['Error sending email', error, params.data])
+        route.renderError('Error sending email', error, params.data)
         return false
       } else {
         log.success('Successfully sent email')
         database.delete(params.id, function (error) {
           if (error) {
-            log.error(['Error deleting from queue', error, event])
+            route.renderError('Error deleting from queue', error, event)
             return false
           } else {
             log.success('Successfully deleted queue item')
