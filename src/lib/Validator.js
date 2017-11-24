1050ba7b499e230fc824f1b88a0d263a69295eb7
Daniel Ireson <danielireson@gmail.com>
2017-11-24 23:45:20 +0000

    add validator class

---
+++ src/lib/Validator.js
@@ -0,0 +1,13 @@
+class Validator {
+  static isUrl (string) {
+    let regex = /[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+    return regex.test(string)
+  }
+
+  static isEmail (string) {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return regex.test(string)
+  }
+}
+
+module.exports.Validator = Validator
