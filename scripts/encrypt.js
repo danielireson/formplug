496ee39fda6078baa55491eba6021c16249ef9f5
Daniel Ireson <danielireson@gmail.com>
2017-11-26 22:38:34 +0000

    replace encrypt handler with npm run script

---
+++ scripts/encrypt.js
@@ -0,0 +1,21 @@
+const config = require('../config.json')
+const Encrypter = require('../src/lib/Encrypter.js')
+
+const args = process.argv.slice(2)
+
+if (args.length === 0) {
+  console.error('Please provide an email address to encrypt as an argument')
+  console.log("e.g. 'npm run encrypt -- johndoe@example.com'")
+  process.exit(0)
+}
+
+const encrypter = new Encrypter(config.ENCRYPTION_KEY)
+args.forEach(function (plainTextEmail) {
+  try {
+    let encryptedEmail = encrypter.encrypt(plainTextEmail)
+    console.log(`${plainTextEmail} => ${encryptedEmail}`)
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`)
+    console.log('Please ensure you have set ENCRYPTION_KEY in config.json')
+  }
+})
