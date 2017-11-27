2c0ec97f65f4a1284f603950e6788ba07e9609f2
Daniel Ireson <danielireson@gmail.com>
2017-11-27 22:09:29 +0000

    update Encrypter to swallow any errors that are thrown

--- src/lib/Encrypter.js
+++ src/lib/Encrypter.js
@@ -8,21 +8,23 @@ class Encrypter {
   encrypt (input) {
     this._validateEncryptionKey()
 
-    let cipher = crypto.createCipher('aes-256-ctr', this.encryptionKey)
-    let crypted = cipher.update(input, 'utf8', 'hex')
-    crypted += cipher.final('hex')
-
-    return crypted
+    try {
+      let cipher = crypto.createCipher('aes-256-ctr', this.encryptionKey)
+      let crypted = cipher.update(input, 'utf8', 'hex')
+      crypted += cipher.final('hex')
+      return crypted
+    } catch (e) {}
   }
 
   decrypt (input) {
     this._validateEncryptionKey()
 
-    let decipher = crypto.createDecipher('aes-256-ctr', this.encryptionKey)
-    let text = decipher.update(input, 'hex', 'utf8')
-    text += decipher.final('utf8')
-
-    return text
+    try {
+      let decipher = crypto.createDecipher('aes-256-ctr', this.encryptionKey)
+      let text = decipher.update(input, 'hex', 'utf8')
+      text += decipher.final('utf8')
+      return text
+    } catch (e) {}
   }
 
   _validateEncryptionKey () {
