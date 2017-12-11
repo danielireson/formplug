4588ced5811162c84f3b78f79f5b6cb3bf47701d
Daniel Ireson <danielireson@gmail.com>
2017-12-11 22:08:38 +0000

    remove describe nesting from Validator

--- src/lib/Validator.spec.js
+++ src/lib/Validator.spec.js
@@ -5,62 +5,59 @@ const assert = require('chai').assert
 const Validator = require('./Validator')
 
 describe('Validator', function () {
-  describe('Email', function () {
-    it('should validate an email address', function () {
-      assert.isTrue(Validator.isEmail('johndoe@example.com'))
-    })
-
-    it('should require a username', function () {
-      assert.isNotTrue(Validator.isEmail('@example'))
-    })
-
-    it('should require a domain', function () {
-      assert.isNotTrue(Validator.isEmail('johndoe'))
-    })
-
-    it('should require a tld', function () {
-      assert.isNotTrue(Validator.isEmail('johndoe.example'))
-    })
-
-    it('should allow a full stop', function () {
-      assert.isTrue(Validator.isEmail('john.doe@example.com'))
-    })
-
-    it('should validate with a dash', function () {
-      assert.isTrue(Validator.isEmail('john-doe@example.com'))
-    })
-
-    it('should validate with a plus sign', function () {
-      assert.isTrue(Validator.isEmail('john+doe@example.com'))
-    })
-
-    it('should validate with a uppercase letters', function () {
-      assert.isTrue(Validator.isEmail('jOhNdOe@eXaMpLe.com'))
-    })
-
-    it('should not allow a space', function () {
-      assert.isNotTrue(Validator.isEmail('john doe@example.com'))
-    })
+  it('should validate an email address', function () {
+    assert.isTrue(Validator.isEmail('johndoe@example.com'))
   })
-  describe('Website', function () {
-    it('should validate a http website', function () {
-      assert.isTrue(Validator.isWebsite('http://example.com'))
-    })
 
-    it('should validate a https website', function () {
-      assert.isTrue(Validator.isWebsite('https://example.com'))
-    })
+  it('should require emails have a username', function () {
+    assert.isNotTrue(Validator.isEmail('@example'))
+  })
 
-    it('should validate without a protocol', function () {
-      assert.isTrue(Validator.isWebsite('example.com'))
-    })
+  it('should require emails have a domain', function () {
+    assert.isNotTrue(Validator.isEmail('johndoe'))
+  })
 
-    it('should require a domain', function () {
-      assert.isNotTrue(Validator.isWebsite('https://'))
-    })
+  it('should require emails have a tld', function () {
+    assert.isNotTrue(Validator.isEmail('johndoe.example'))
+  })
 
-    it('should require a tld', function () {
-      assert.isNotTrue(Validator.isWebsite('https://example'))
-    })
+  it('should allow emails with a full stop', function () {
+    assert.isTrue(Validator.isEmail('john.doe@example.com'))
+  })
+
+  it('should allow emails with a dash', function () {
+    assert.isTrue(Validator.isEmail('john-doe@example.com'))
+  })
+
+  it('should allow emails with a plus sign', function () {
+    assert.isTrue(Validator.isEmail('john+doe@example.com'))
+  })
+
+  it('should allow emails with uppercase letters', function () {
+    assert.isTrue(Validator.isEmail('jOhNdOe@eXaMpLe.com'))
+  })
+
+  it('should not allow emails with a space', function () {
+    assert.isNotTrue(Validator.isEmail('john doe@example.com'))
+  })
+
+  it('should validate a http website', function () {
+    assert.isTrue(Validator.isWebsite('http://example.com'))
+  })
+
+  it('should validate a https website', function () {
+    assert.isTrue(Validator.isWebsite('https://example.com'))
+  })
+
+  it('should validate a website without a protocol', function () {
+    assert.isTrue(Validator.isWebsite('example.com'))
+  })
+
+  it('should require a website domain', function () {
+    assert.isNotTrue(Validator.isWebsite('https://'))
+  })
+
+  it('should require a website tld', function () {
+    assert.isNotTrue(Validator.isWebsite('https://example'))
   })
 })
