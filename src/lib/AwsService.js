fc6941a0eae511f154c5b27c13b0ce59c67a1373
Daniel Ireson <danielireson@gmail.com>
2017-12-16 16:51:07 +0000

    add AwsService class

---
+++ src/lib/AwsService.js
@@ -0,0 +1,20 @@
+class AwsService {
+  constructor (aws) {
+    this.aws = aws
+  }
+
+  invokeLambda (serviceName, stage, functionName, payload) {
+    let event = {
+      FunctionName: `${serviceName}-${stage}-${functionName}`,
+      InvocationType: 'Event',
+      Payload: JSON.stringify(payload)
+    }
+    return new this.aws.Lambda().invoke(event).promise()
+  }
+
+  sendEmail (email) {
+    return new this.aws.SES().sendEmail(email).promise()
+  }
+}
+
+module.exports = AwsService
