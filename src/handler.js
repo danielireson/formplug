ab91f052db4a35f0bdab1c46285cfc75f80ddb83
Daniel Ireson <daniel.ireson@zestia.com>
2019-03-26 21:17:34 +0000

    add empty lines in handler for readability

--- src/handler.js
+++ src/handler.js
@@ -19,32 +19,45 @@ module.exports.handle = (event, context, callback) => {
 
   request.validate()
     .then(function () {
-      let recipientCount = [].concat(request.recipients.cc, request.recipients.bcc, request.recipients.replyTo).length
+      let recipientCount = [].concat(
+        request.recipients.cc,
+        request.recipients.bcc,
+        request.recipients.replyTo).length
+
       Log.info(`sending to '${request.recipients.to}' and ${recipientCount} other recipients`)
+
       const email = new Email(
         configService.getValue('SENDER_ARN'),
         configService.getValueWithDefault('MSG_SUBJECT', 'You have a form submission'))
+
       return emailService.send(email.build(request.recipients, request.userParameters))
     })
     .then(function () {
       const statusCode = request.redirectUrl ? 302 : 200
-      const message = configService.getValueWithDefault('MSG_RECEIVE_SUCCESS', 'Form submission successfully made')
+      const message = configService.getValueWithDefault(
+        'MSG_RECEIVE_SUCCESS',
+        'Form submission successfully made')
+
       return Promise.resolve(new Response(statusCode, message))
     })
     .catch(function (error) {
       Log.error('error was caught while executing receive lambda', error)
+
       return Promise.resolve(new Response(error.statusCode, error.message))
     })
     .then(function (response) {
       Log.info(`returning http ${response.statusCode} response`)
+
       if (request.responseFormat === 'json') {
         callback(null, response.buildJson())
         return
       }
+
       if (request.responseFormat === 'plain' && request.redirectUrl) {
         callback(null, response.buildRedirect(request.redirectUrl))
         return
       }
+
       if (request.responseFormat === 'html') {
         const template = fs.readFileSync(path.resolve(__dirname, 'template.html')).toString()
         callback(null, response.buildHtml(template))
