e7bd89f79493cd5e72a1c68a38c93978640e2cc4
Daniel Ireson <daniel.ireson@zestia.com>
2019-07-23 22:20:08 +0100

    use async await over chained promises

--- src/handler.js
+++ src/handler.js
@@ -14,80 +14,69 @@ const emailService = require('./services/EmailService')
 const config = require('./utils/config')
 const logging = require('./utils/logging')
 
-module.exports.handle = (event, context, callback) => {
+module.exports.handle = async (event, context, callback) => {
   const encrypter = new Encrypter(config.getValue('ENCRYPTION_KEY'))
   const request = new Request(event, encrypter)
 
-  const paramCount = Object.keys(request.userParameters).length
-  logging.info(`${request.responseFormat} request received with ${paramCount} parameters`)
+  let response = null
 
-  request.validate()
-    .then(function () {
-      const recipientCount = [].concat(
-        request.recipients.cc,
-        request.recipients.bcc,
-        request.recipients.replyTo).length
+  try {
+    await request.validate()
 
-      logging.info(`sending to '${request.recipients.to}' and ${recipientCount} other recipients`)
+    const recipientCount = [].concat(
+      request.recipients.cc,
+      request.recipients.bcc,
+      request.recipients.replyTo).length
 
-      const email = new Email(
-        config.getValue('SENDER'),
-        config.getValue('SENDER_ARN'),
-        config.getValueWithDefault('MSG_SUBJECT', 'You have a form submission'))
+    logging.info(`sending to '${request.recipients.to}' and ${recipientCount} other recipients`)
 
-      return emailService.send(email.build(request.recipients, request.userParameters))
-    })
-    .then(function () {
-      const message = config.getValueWithDefault(
-        'MSG_RECEIVE_SUCCESS',
-        'Form submission successfully made')
+    const email = new Email(
+      config.getValue('SENDER'),
+      config.getValue('SENDER_ARN'),
+      config.getValueWithDefault('MSG_SUBJECT', 'You have a form submission'))
 
-      let response = null
+    await emailService.send(email.build(request.recipients, request.userParameters))
 
-      if (request.responseFormat === 'json') {
-        response = new JsonResponse(200, message)
-      } else if (request.redirectUrl) {
-        response = new RedirectResponse(302, message, request.redirectUrl)
-      } else {
-        try {
-          response = new HtmlResponse(200, message, htmlTemplate())
-        } catch (error) {
-          response = new PlainTextResponse(500, message)
-        }
+    const message = config.getValueWithDefault(
+      'MSG_RECEIVE_SUCCESS',
+      'Form submission successfully made')
+
+    if (request.responseFormat === 'json') {
+      response = new JsonResponse(200, message)
+    } else if (request.redirectUrl) {
+      response = new RedirectResponse(302, message, request.redirectUrl)
+    } else {
+      try {
+        response = new HtmlResponse(200, message, htmlTemplate())
+      } catch (error) {
+        response = new PlainTextResponse(500, message)
       }
+    }
+  } catch (error) {
+    logging.error('error was caught while executing receive lambda', error)
 
-      return Promise.resolve(response)
-    })
-    .catch(function (error) {
-      logging.error('error was caught while executing receive lambda', error)
+    let statusCode = 500
+    let message = 'An unexpected error occurred'
 
-      let statusCode = 500
-      let message = 'An unexpected error occurred'
+    if (error instanceof HttpError) {
+      statusCode = error.statusCode
+      message = error.message
+    }
 
-      if (error instanceof HttpError) {
-        statusCode = error.statusCode
-        message = error.message
+    if (request.responseFormat === 'json') {
+      response = new JsonResponse(statusCode, message)
+    } else {
+      try {
+        response = new HtmlResponse(statusCode, message, htmlTemplate())
+      } catch (error) {
+        response = new PlainTextResponse(statusCode, message)
       }
+    }
+  }
 
-      let response = null
+  logging.info(`returning http ${response.statusCode} response`)
 
-      if (request.responseFormat === 'json') {
-        response = new JsonResponse(statusCode, message)
-      } else {
-        try {
-          response = new HtmlResponse(statusCode, message, htmlTemplate())
-        } catch (error) {
-          response = new PlainTextResponse(statusCode, message)
-        }
-      }
-
-      return Promise.resolve(response)
-    })
-    .then(function (response) {
-      logging.info(`returning http ${response.statusCode} response`)
-
-      callback(null, response.build())
-    })
+  callback(null, response.build())
 }
 
 function htmlTemplate () {
