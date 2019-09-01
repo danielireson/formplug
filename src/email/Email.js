11332955e484206f3ff9c7625c0ffe058202c422
Daniel Ireson <daniel.ireson@zestia.com>
2019-09-01 20:39:32 +0100

    use validate method on email model

--- src/email/Email.js
+++ src/email/Email.js
@@ -1,65 +1,76 @@
+const InternalServerError = require('../error/InternalServerError')
+
 const validation = require('../utils/validation')
 
 class Email {
-  constructor (sender, senderArn, subject) {
-    this.sender = sender
-    this.senderArn = senderArn
-    this.subject = subject
-  }
-
-  build (recipients, userParameters) {
-    this._validateArn()
-
-    return {
-      Source: this._buildSenderSource(),
-      ReplyToAddresses: recipients.replyTo,
-      Destination: {
-        ToAddresses: [recipients.to],
-        CcAddresses: recipients.cc,
-        BccAddresses: recipients.bcc
+  constructor (sender, senderArn, subject, recipients, userParameters) {
+    this.Source = this._buildSenderSource(sender, senderArn)
+    this.ReplyToAddresses = recipients.replyTo,
+    this.Destination = {
+      ToAddresses: [recipients.to],
+      CcAddresses: recipients.cc,
+      BccAddresses: recipients.bcc
+    }
+    this.Message = {
+      Subject: {
+        Data: subject
       },
-      Message: {
-        Subject: {
-          Data: this.subject
-        },
-        Body: {
-          Text: {
-            Data: this._buildMessageBody(userParameters)
-          }
+      Body: {
+        Text: {
+          Data: this._buildMessageBody(userParameters)
         }
       }
+    }    
+  }
+
+  validate () {
+    const sourceEmail = this.Source.match(/<(.)+>/gi)
+
+    if (!sourceEmail || sourceEmail.length === 0) {
+      return new InternalServerError('Source should contain a valid email address')
+    }
+
+    if (!this.Message.Subject.Data || this.Message.Subject.Data.length === 0) {
+      return new InternalServerError('Subject is invalid')
+    }
+
+    if (!this.Message.Body.Text.Data || this.Message.Body.Text.Data.length === 0) {
+      return new InternalServerError('Body is invalid')
+    }
+
+    const invalidReplyToEmail = this.ReplyToAddresses.find(e => !validation.isEmail(e))
+
+    if (invalidReplyToEmail) {
+      return new InternalServerError(`Invalid reply to recipient: ${invalidReplyToEmail}`)
+    }
+
+    const invalidToEmail = this.Destination.ToAddresses.find(e => !validation.isEmail(e))
+
+    if (invalidToEmail) {
+      return new InternalServerError(`Invalid to recipient: ${invalidToEmail}`)
+    }
+
+    const invalidCcEmail = this.Destination.CcAddresses.find(e => !validation.isEmail(e))
+
+    if (invalidCcEmail) {
+      return new InternalServerError(`Invalid cc recipient: ${invalidCcEmail}`)
+    }
+
+    const invalidBccEmail = this.Destination.BccAddresses.find(e => !validation.isEmail(e))
+
+    if (invalidBccEmail) {
+      return new InternalServerError(`Invalid bcc recipient: ${invalidBccEmail}`)
     }
   }
 
-  _validateArn () {
-    let senderArnAsArray = this.senderArn.split(':')
-    let identity = senderArnAsArray.length > 0 ? senderArnAsArray[senderArnAsArray.length - 1] : ''
-
-    if (senderArnAsArray.length !== 6) {
-      throw new Error('sender ARN is formatted incorrectly')
-    }
-
-    if (this.senderArn.substring(0, 3) !== 'arn') {
-      throw new Error("sender ARN should start with 'arn'")
-    }
-
-    if (identity.length < 9) {
-      throw new Error('sender ARN identity length is invalid')
-    }
-
-    if (!validation.isEmail(identity.substring(9))) {
-      throw new Error('sender ARN identity email address is invalid')
-    }
-  }
-
-  _buildSenderSource () {
-    let senderArnAsArray = this.senderArn.split('/')
-    let email = senderArnAsArray[senderArnAsArray.length - 1]
-    return `${this.sender} <${email}>`
+  _buildSenderSource (sender, senderArn) {
+    const senderArnAsArray = (senderArn || '').split('/')
+    const email = senderArnAsArray[senderArnAsArray.length - 1]
+    return `${sender} <${email}>`
   }
 
   _buildMessageBody (userParameters) {
-    return Object.keys(userParameters)
+    return Object.keys(userParameters || {})
       .filter(function (param) {
         // don't send private variables
         return param.substring(0, 1) !== '_'
