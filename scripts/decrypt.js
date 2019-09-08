94b2180bd651241fe7b228dcb835fba49f712f28
Daniel Ireson <daniel.ireson@zestia.com>
2019-09-08 21:20:55 +0100

    update encryption scripts for refactored project

--- scripts/decrypt.js
+++ scripts/decrypt.js
@@ -1,21 +1,23 @@
 const config = require('../config.json')
-const Encrypter = require('../src/common/Encrypter.js')
+const { decrypt } = require('../src/lib/encryption.js')
 
 const args = process.argv.slice(2)
 
 if (args.length === 0) {
   console.error('Please provide an email address to decrypt as an argument')
-  console.log("e.g. 'npm run decrypt -- johndoe@example.com'")
   process.exit(0)
 }
 
-const encrypter = new Encrypter(config.ENCRYPTION_KEY)
-args.forEach(function (encryptedEmail) {
+if (!config.ENCRYPTION_KEY) {
+  console.error('Please set the ENCRYPTION_KEY config variable')
+  process.exit(0)
+}
+
+args.forEach(encryptedEmail => {
   try {
-    let plainTextEmail = encrypter.decrypt(encryptedEmail)
+    let plainTextEmail = decrypt(encryptedEmail, config.ENCRYPTION_KEY)
     console.log(`${encryptedEmail} => ${plainTextEmail}`)
   } catch (error) {
     console.error(`An error occurred: ${error.message}`)
-    console.log('Please ensure you have set ENCRYPTION_KEY in config.json')
   }
 })
--- scripts/encrypt.js
+++ scripts/encrypt.js
@@ -1,21 +1,23 @@
 const config = require('../config.json')
-const Encrypter = require('../src/common/Encrypter.js')
+const { encrypt } = require('../src/lib/encryption.js')
 
 const args = process.argv.slice(2)
 
 if (args.length === 0) {
   console.error('Please provide an email address to encrypt as an argument')
-  console.log("e.g. 'npm run encrypt -- johndoe@example.com'")
   process.exit(0)
 }
 
-const encrypter = new Encrypter(config.ENCRYPTION_KEY)
-args.forEach(function (plainTextEmail) {
+if (!config.ENCRYPTION_KEY) {
+  console.error('Please set the ENCRYPTION_KEY config variable')
+  process.exit(0)
+}
+
+args.forEach(plainTextEmail => {
   try {
-    let encryptedEmail = encrypter.encrypt(plainTextEmail)
+    let encryptedEmail = encrypt(plainTextEmail, config.ENCRYPTION_KEY)
     console.log(`${plainTextEmail} => ${encryptedEmail}`)
   } catch (error) {
     console.error(`An error occurred: ${error.message}`)
-    console.log('Please ensure you have set ENCRYPTION_KEY in config.json')
   }
 })
