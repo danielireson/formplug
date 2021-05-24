94e79125669aa2074b2629053c839b4d92928233
Daniel Ireson <danielireson@gmail.com>
2021-05-24 15:28:34 +0100

    remove fast-check dependency
    This is only used in one place and is not necessary so removing.

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -1,12 +1,12 @@
-const VALID_WEBSITE_SCHEMES = new Set(["http:", "https:"]);
+const VALID_WEBSITE_PROTOCOLS = new Set(["http:", "https:"]);
 
 module.exports.isWebsite = (string) => {
   try {
     const parsed = new URL(string);
-    return VALID_WEBSITE_SCHEMES.has(parsed.protocol);
+    return VALID_WEBSITE_PROTOCOLS.has(parsed.protocol);
   } catch (e) {
-    // Keeping the regex validation as a fallback to avoid
-    // a backwards-incompatible behavior change for incomplete URLs.
+    // Keeping the regex validation as a fallback to avoid a
+    // backwards-incompatible behavior change for incomplete URLs
     return /[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/.test(
       string
     );
