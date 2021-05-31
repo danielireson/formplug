5db2d4ae2d59fcfb4e9358f493c3908b1107ef0d
Daniel Ireson <danielireson@gmail.com>
2021-05-31 19:56:51 +0100

    use non-depreciated iv-based encryption

--- src/lib/encryption.js
+++ src/lib/encryption.js
@@ -1,23 +1,40 @@
 const crypto = require("crypto");
 
+const ivLength = 16;
+const saltLength = 64;
+const tagLength = 16;
+const tagPosition = saltLength + ivLength;
+const inputPosition = tagPosition + tagLength;
+
 module.exports.encrypt = (input, encryptionKey) => {
   try {
-    let cipher = crypto.createCipher("aes-256-ctr", encryptionKey);
-    let crypted = cipher.update(input, "utf8", "hex");
-    crypted += cipher.final("hex");
-    return crypted;
-  } catch (e) {
+    const iv = crypto.randomBytes(ivLength);
+    const salt = crypto.randomBytes(saltLength);
+    const key = crypto.pbkdf2Sync(encryptionKey, salt, 100000, 32, "sha512");
+    const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
+    const encrypted = Buffer.concat([
+      cipher.update(input, "utf8"),
+      cipher.final(),
+    ]);
+    const tag = cipher.getAuthTag();
+    return Buffer.concat([salt, iv, tag, encrypted]).toString("hex");
+  } catch (error) {
     return "";
   }
 };
 
-module.exports.decrypt = (input, encryptionKey) => {
+module.exports.decrypt = (output, encryptionKey) => {
   try {
-    let decipher = crypto.createDecipher("aes-256-ctr", encryptionKey);
-    let text = decipher.update(input, "hex", "utf8");
-    text += decipher.final("utf8");
-    return text;
-  } catch (e) {
+    const stringValue = Buffer.from(output, "hex");
+    const salt = stringValue.slice(0, saltLength);
+    const iv = stringValue.slice(saltLength, tagPosition);
+    const tag = stringValue.slice(tagPosition, inputPosition);
+    const encrypted = stringValue.slice(inputPosition);
+    const key = crypto.pbkdf2Sync(encryptionKey, salt, 100000, 32, "sha512");
+    const decipher = crypto.createDecipheriv("aes-256-gcm", key, iv);
+    decipher.setAuthTag(tag);
+    return decipher.update(encrypted) + decipher.final("utf8");
+  } catch (error) {
     return "";
   }
 };
