6ea59f88b8217329153813df31ae08ba1310aaf9
Daniel Ireson <danielireson@gmail.com>
2021-06-26 22:27:46 +0100

    remove redundant email validation

--- src/email/Email.js
+++ src/email/Email.js
@@ -1,6 +1,3 @@
-const { isEmail } = require("validator");
-const InternalServerError = require("../error/InternalServerError");
-
 class Email {
   constructor(sender, senderArn, subject, recipients, params) {
     this.Source = this._source(sender, senderArn);
@@ -20,61 +17,6 @@ class Email {
         },
       },
     };
-  }
-
-  validate() {
-    const sourceEmail = this.Source.match(/<(.)+>/gi);
-
-    if (!sourceEmail || sourceEmail.length === 0) {
-      return new InternalServerError(
-        "Source should contain a valid email address"
-      );
-    }
-
-    if (!this.Message.Subject.Data || this.Message.Subject.Data.length === 0) {
-      return new InternalServerError("Subject is invalid");
-    }
-
-    if (
-      !this.Message.Body.Text.Data ||
-      this.Message.Body.Text.Data.length === 0
-    ) {
-      return new InternalServerError("Body is invalid");
-    }
-
-    const invalidReplyToEmail = this.ReplyToAddresses.find((e) => !isEmail(e));
-
-    if (invalidReplyToEmail) {
-      return new InternalServerError(
-        `Invalid reply to recipient: ${invalidReplyToEmail}`
-      );
-    }
-
-    const invalidToEmail = this.Destination.ToAddresses.find(
-      (e) => !isEmail(e)
-    );
-
-    if (invalidToEmail) {
-      return new InternalServerError(`Invalid to recipient: ${invalidToEmail}`);
-    }
-
-    const invalidCcEmail = this.Destination.CcAddresses.find(
-      (e) => !isEmail(e)
-    );
-
-    if (invalidCcEmail) {
-      return new InternalServerError(`Invalid cc recipient: ${invalidCcEmail}`);
-    }
-
-    const invalidBccEmail = this.Destination.BccAddresses.find(
-      (e) => !isEmail(e)
-    );
-
-    if (invalidBccEmail) {
-      return new InternalServerError(
-        `Invalid bcc recipient: ${invalidBccEmail}`
-      );
-    }
   }
 
   _source(sender, senderArn) {
